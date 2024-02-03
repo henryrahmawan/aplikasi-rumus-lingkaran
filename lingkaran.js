b1859be889984453614e22d1d010b46d5e62d3d9
ghiraihan <graihan@binaracademy.org>
2024-02-03 21:20:20 +0700

    Penambahan Rumus Lingkaran

---
+++ lingkaran.js
@@ -0,0 +1,17 @@
+function TentukanLuasDanKelilingSegitigaSamaSisi (a,t) {
+  const alas = a;
+  const tinggi = t;
+ 
+ //Proses//
+  const Luas = (alas*tinggi)/2
+  const Keliling = 3*alas
+ 
+ //Hasil//
+  console.log("Alas:", alas,"cm");
+  console.log("Tinggi:", tinggi,"cm");
+  console.log("Luas:", Luas,"cm^2");
+  console.log("Keliling Segitiga:", Keliling,"cm");
+ }
+ 
+ TentukanLuasDanKelilingSegitigaSamaSisi (5, 10);
+ 
